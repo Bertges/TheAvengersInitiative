cde93d7111c0d50819f19f384b84d54f44c34fde
matheusmendesp <61604854+matheusmendesp@users.noreply.github.com>
2021-06-09 12:03:08 -0300

    api url

--- TheAvengersInitiative/ViewController.swift
+++ TheAvengersInitiative/ViewController.swift
@@ -11,5 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		guard let apiUrl: String = Bundle.main.infoDictionary?["API_URL"] as? String else { return }
+		
+		debugPrint(apiUrl)
     }
 }
