75cd0836f439e6f42b0c3ff1e7c792b6bc2014a4
Sara Felix <sarabfelix1@gmail.com>
2021-06-17 11:57:30 -0300

    edit count
    add changes

--- TheAvengersInitiative/CharactersList/Views/CharacterListCell.swift
+++ TheAvengersInitiative/CharactersList/Views/CharacterListCell.swift
@@ -14,14 +14,20 @@ final class CharacterListCell: UITableViewCell, ViewCode {
         let imageView: UIImageView = .init()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.backgroundColor = .red
+        imageView.layer.cornerRadius = 5
         return imageView
     }()
 
     let characterNameLabel: UILabel = {
         let label: UILabel = .init(frame: .zero)
         label.text = "Nome do Personagem"
-        label.backgroundColor = .blue
+        label.backgroundColor = .white
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 6
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 2
         return label
     }()
 
@@ -29,7 +35,13 @@ final class CharacterListCell: UITableViewCell, ViewCode {
         let label: UILabel = .init(frame: .zero)
         label.text = "222"
         label.backgroundColor = .gray
+        label.textAlignment = .center
+        label.backgroundColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 2
         return label
     }()
 
@@ -52,8 +64,12 @@ final class CharacterListCell: UITableViewCell, ViewCode {
         characterImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
 
         characterNameLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 8).isActive = true
-        characterNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
+        characterNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -52).isActive = true
         characterNameLabel.bottomAnchor.constraint(equalTo: characterImageView.bottomAnchor).isActive = true
+        
+        characterComicsCount.leadingAnchor.constraint(equalTo: characterNameLabel.trailingAnchor, constant: 8).isActive = true
+        characterComicsCount.topAnchor.constraint(greaterThanOrEqualTo: characterNameLabel.topAnchor).isActive = true
+        characterComicsCount.widthAnchor.constraint(equalToConstant: 40).isActive = true
     }
 
     func configureViews() {
