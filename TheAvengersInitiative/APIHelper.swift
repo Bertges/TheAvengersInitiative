863cc8ed26a7981ed2a825a6bed1345a02211a05
Bertges <bertges.felippe@gmail.com>
2021-06-30 11:37:28 -0300

    Limpa gitignore momentaneamente

---
+++ TheAvengersInitiative/APIHelper.swift
@@ -0,0 +1,13 @@
+//
+//  APIHelper.swift
+//  TheAvengersInitiative
+//
+//  Created by Felippe Bertges on 24/06/21.
+//
+
+import Foundation
+
+final class APIHelper {
+    static let marvelAPIKey: String = ""
+    static let marvelPrivateAPIKey: String = ""
+}
